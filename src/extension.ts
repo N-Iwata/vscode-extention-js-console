d9d3a046424ce621f280aaba1766942cfbda2196
Nobuhito Iwata <nobuhito323@gmail.com>
2021-09-05 22:33:38 +0900

    Add insert console log statement

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const insertText = (val: string) => {
+  const editor = vscode.window.activeTextEditor;
+
+  if (!editor) {
+    vscode.window.showErrorMessage("Can't insert log because no document is open");
+    return;
+  }
+
+  const selection = editor.selection;
+  const range = new vscode.Range(selection.start, selection.end);
+
+  editor.edit((editBuilder) => {
+    editBuilder.replace(range, val);
+  });
+};
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "js-console" is now active!');
+  console.log('Congratulations, your extension "js-console" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('js-console.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from js-console!');
-	});
+  const insertConsoleLog = vscode.commands.registerCommand("js-console.insertConsoleLog", () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
 
-	context.subscriptions.push(disposable);
+    const selection = editor.selection;
+    const text = editor.document.getText(selection);
+    text
+      ? vscode.commands.executeCommand("editor.action.insertLineAfter").then(() => {
+          const logToInsert = `console.log('${text}: ', ${text});`;
+          insertText(logToInsert);
+        })
+      : insertText("console.log();");
+  });
+
+  context.subscriptions.push(insertConsoleLog);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
