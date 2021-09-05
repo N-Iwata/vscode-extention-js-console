1b2a73bfdef6df35ee2ef72f79db0496dba18c66
Nobuhito Iwata <nobuhito323@gmail.com>
2021-09-05 22:53:34 +0900

    Refactor async await

--- src/extension.ts
+++ src/extension.ts
@@ -20,28 +20,9 @@ export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "js-console" is now active!');
 
   // insert console.log()
-  const insertConsoleLog = vscode.commands.registerCommand("js-console.insertConsoleLog", () => {
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return;
-    }
-
-    const selection = editor.selection;
-    const text = editor.document.getText(selection);
-    text
-      ? vscode.commands.executeCommand("editor.action.insertLineAfter").then(() => {
-          const logToInsert = `console.log('${text}: ', ${text});`;
-          insertText(logToInsert);
-        })
-      : insertText("console.log();");
-  });
-
-  context.subscriptions.push(insertConsoleLog);
-
-  // insert console.table()
-  const insertConsoleTable = vscode.commands.registerCommand(
-    "js-console.insertConsoleTable",
-    () => {
+  const insertConsoleLog = vscode.commands.registerCommand(
+    "js-console.insertConsoleLog",
+    async () => {
       const editor = vscode.window.activeTextEditor;
       if (!editor) {
         return;
@@ -49,12 +30,39 @@ export function activate(context: vscode.ExtensionContext) {
 
       const selection = editor.selection;
       const text = editor.document.getText(selection);
-      text
-        ? vscode.commands.executeCommand("editor.action.insertLineAfter").then(() => {
-            const logToInsert = `console.table('${text}: ', ${text});`;
-            insertText(logToInsert);
-          })
-        : insertText("console.table();");
+
+      if (text) {
+        await vscode.commands.executeCommand("editor.action.insertLineAfter");
+        const consoleText = `console.log('${text}: ', ${text});`;
+        insertText(consoleText);
+      } else {
+        const consoleText = "console.log();";
+        insertText(consoleText);
+      }
+    }
+  );
+
+  context.subscriptions.push(insertConsoleLog);
+
+  // insert console.table()
+  const insertConsoleTable = vscode.commands.registerCommand(
+    "js-console.insertConsoleTable",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+
+      const selection = editor.selection;
+      const text = editor.document.getText(selection);
+      if (text) {
+        await vscode.commands.executeCommand("editor.action.insertLineAfter");
+        const consoleText = `console.table('${text}: ', ${text});`;
+        insertText(consoleText);
+      } else {
+        const consoleText = "console.table();";
+        insertText(consoleText);
+      }
     }
   );
 
